8cb49929ac6e11fdcfcd4505357183f986b35f08
EmptyBottle <tts13511@hotmail.com>
2025-10-26 13:35:10 +0900

    App.tsxを使用して画面を表示するよう変更

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import UploadForm from './UploadForm'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <UploadForm />
+        <App />
     </StrictMode>
 )
