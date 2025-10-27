f091c9c325b74cd57f6652bc86f040a6a05e0916
EmptyBottle <tts13511@hotmail.com>
2025-10-27 15:35:05 +0900

    表示・非表示を切り替えられるボックスを追加

--- src/config/ConfigCard.tsx
+++ src/config/ConfigCard.tsx
@@ -1,15 +1,31 @@
+import { JSX, useState } from "react";
 import DisplayConfig from "./DisplayConfig";
-
+import "./ConfigCard.css"
 
 type ConfigCardProps = {
     config: DisplayConfig
 }
 
 const ConfigCard = (props: ConfigCardProps) => {
+    const [show, setShow] = useState(false);
+
     return (
         <div className="card">
+            <h2>詳細設定</h2>
+            <ToggleBox title="名前を読み替える" elem={<p>todo</p>} />
         </div>
     )
+}
+
+const ToggleBox = (props: { title: string, elem: JSX.Element }) => {
+    const [show, setShow] = useState(false);
+
+    return (
+        <div>
+            <p className="toggleButton" onClick={() => setShow(!show)}>{`${show ? "▼" : "▶"} ${props.title}`}</p>
+            {show ? <div className="toggleBox">{props.elem}</div> : null}
+        </div>
+    );
 }
 
 export default ConfigCard;
