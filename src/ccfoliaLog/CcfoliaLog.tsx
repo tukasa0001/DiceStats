bf557169660cb8bcb54373cda8c144f1d77a0c90
EmptyBottle <tts13511@hotmail.com>
2025-10-28 14:24:19 +0900

    技能振りの判定を正規表現で行うよう変更

--- src/ccfoliaLog/CcfoliaLog.tsx
+++ src/ccfoliaLog/CcfoliaLog.tsx
@@ -16,6 +16,7 @@ const parseCcfoliaLog = (log: string): CcfoliaMessage[] => {
         const channel = span[0].textContent.substring(2, span[0].textContent.length - 1);
         const name = span[1].textContent.trim();
         const text = span[2].textContent.trim();
+        let reg: RegExpMatchArray | null = null;
 
         if (name === "system") {
             // [ {name} ] {param} : {prev} → {value}
@@ -23,13 +24,13 @@ const parseCcfoliaLog = (log: string): CcfoliaMessage[] => {
             if (regex === null) continue;
             msgs.push(new ParamChangeMessage(channel, regex[1], regex[2], Number(regex[3]), Number(regex[4])));
         }
-        else if (text.startsWith("CCB<=")) {
+        else if (reg = text.match(/^(S|s)?(CCB|ccb)<=([0-9]+)\s*(【.*】)?\s*\(1D100<=[0-9]+\) ＞ ([0-9]+) ＞/)) {
             // CCB<={successValue} 【{skillName}】 (1D100<={successValue}) ＞ {diceValue} ＞ {結果}
-            const successValue = Number(text.match(/CCB<=([0-9]*)/)?.[1] ?? "0");
-            const skillName = text.match(/【(.*)】/)?.[1] ?? "";
-            const diceValue = Number(text.match(/＞ ([0-9]*) ＞/)?.[1] ?? "0");
+            const successValue = Number(reg[3]);
+            const skillName = reg[4] ?? "不明な技能";
+            const diceValue = Number(reg[5]);
 
-            msgs.push(new CoCSkillRollMessage(channel, name, skillName === "" ? "不明な技能" : skillName, diceValue, successValue));
+            msgs.push(new CoCSkillRollMessage(channel, name, skillName, diceValue, successValue));
         }
         else if (text.match(/1d100<=[0-9]+ 【正気度ロール】/)) {
             // 1d100<={successValue} 【正気度ロール】 (1D100<={successValue}) ＞ {diceValue} ＞ 成功
