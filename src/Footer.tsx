f1a9676a30f9ac8ead9a2f217e662ec9b8043b6f
EmptyBottle <tts13511@hotmail.com>
2025-10-29 16:40:12 +0900

    フッターを追加

---
+++ src/Footer.tsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="card footer">
+            <div>
+                <p>© 2025 EmptyBottle</p>
+            </div>
+            <div>
+                <p><a href="https://github.com/tukasa0001/DiceStats" target="_blank">GitHub</a></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
