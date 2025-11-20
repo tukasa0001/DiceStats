5e471509547d55d2bb340b337ecce8e071e02084
EmptyBottle <tts13511@hotmail.com>
2025-11-20 11:29:28 +0900

    画面の任意の場所へのドロップへ反応するイベントハンドラを追加

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,45 @@ import ConfigCard from './config/ConfigCard';
 import DisplayConfig from './config/DisplayConfig';
 import Footer from './Footer';
 import { CcfoliaMessage } from './ccfoliaLog/message/CcfoliaMessage';
+import parseCcfoliaLog from './ccfoliaLog/CcfoliaLog';
+import "./UploadArea.css";
 
 const App: FC = () => {
     const [log, setLog] = useState<CcfoliaMessage[] | undefined>(undefined);
     const [config, setConfig] = useState(new DisplayConfig());
+    const [isDropping, setDropping] = useState(false);
 
-    return (
-        <>
+    const onFileChanged = async (file: File) => {
+        const str = await file.text();
+        const parsed = parseCcfoliaLog(str);
+        setLog(parsed);
+    }
+
+    return <>
+        <div onDrop={e => {
+            alert("dropped");
+            setDropping(false);
+            e.preventDefault();
+        }}
+            onDragOver={e => {
+                e.preventDefault();
+            }}
+            onDragEnter={e => setDropping(true)}
+            onDragExit={e => setDropping(false)}>
             <UploadForm onLogFileChanged={setLog} />
             {log !== undefined ? <Stats logFile={log} config={config} /> : ""}
             <ConfigCard log={log} config={config} onConfigChanged={setConfig} />
             <Footer />
-        </>
-    )
+
+            {isDropping ? <div className='upload_area'>
+                <div>
+                    <p>
+                        ファイルをドロップしてアップロード
+                    </p>
+                </div>
+            </div> : null}
+        </div>
+    </>
 }
 
 export default App
