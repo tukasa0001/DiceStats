dc18bb4b9695f7f6e711268a2cd8e8c2ba6c15ae
EmptyBottle <tts13511@hotmail.com>
2025-11-25 15:21:36 +0900

    ErrorBlock/InfoBlockのスタイルを変更

--- src/Utils.tsx
+++ src/Utils.tsx
@@ -1,14 +1,26 @@
-import { Blockquote } from "@radix-ui/themes";
+import { Callout } from "@radix-ui/themes";
+import { InfoIcon, TriangleAlert } from "lucide-react";
 import { JSX } from "react";
 
 export const ErrorBlock = (props: { children: JSX.Element | string }) => {
-    return <Blockquote color="red">
-        {props.children}
-    </Blockquote>
+    return <Callout.Root color="red">
+        <Callout.Icon>
+            <TriangleAlert />
+        </Callout.Icon>
+        <Callout.Text>
+            {props.children}
+        </Callout.Text>
+    </Callout.Root>
 }
 
 export const InfoBlock = (props: { children: JSX.Element | string }) => {
-    return <Blockquote>
-        {props.children}
-    </Blockquote>
+    return <Callout.Root>
+        <Callout.Icon>
+            <InfoIcon />
+        </Callout.Icon>
+        <Callout.Text>
+            {props.children}
+        </Callout.Text>
+    </Callout.Root>
+
 }
