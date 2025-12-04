3105effe48d82fa1ddbdea2875b58e3172ae85b7
EmptyBottle <tts13511@hotmail.com>
2025-12-04 12:35:16 +0900

    バッジを追加

--- src/logView/LogView.tsx
+++ src/logView/LogView.tsx
@@ -1,7 +1,10 @@
-import { Card, Flex } from "@radix-ui/themes";
+import { Badge, Card, Flex } from "@radix-ui/themes";
 import { CcfoliaMessage } from "../ccfoliaLog/message/CcfoliaMessage";
 import { Heading, Text } from "@radix-ui/themes";
 import { TalkMessage } from "../ccfoliaLog/message/TalkMessasge";
+import { CoCSkillRollMessage } from "../ccfoliaLog/message/CoCSkillRollMessage";
+import { SanityCheckMessage } from "../ccfoliaLog/message/SanityCheckMessage";
+import { ParamChangeMessage } from "../ccfoliaLog/message/ParamChangeMessage";
 
 type LogViewProps = {
     logs: CcfoliaMessage[]
@@ -10,9 +13,14 @@ type LogViewProps = {
 export const LogView = (props: LogViewProps) => {
     const { logs } = props;
 
-    return <Flex gap="4" direction="column">
+    return <Flex gap="4" direction="column" mt="4">
         {logs.map((msg, i) => <Card key={i}>
-            <Heading size="4">{msg.sender}</Heading>
+            <Flex gap="2" direction="row">
+                <Heading size="4">{msg.sender}</Heading>
+                {msg instanceof CoCSkillRollMessage ? <Badge color="lime">技能判定</Badge> : null}
+                {msg instanceof SanityCheckMessage ? <Badge color="ruby">SANチェック</Badge> : null}
+                {msg instanceof ParamChangeMessage ? <Badge color="blue">{msg.paramName}変動</Badge> : null}
+            </Flex>
             <Text>{msg.toDisplayText()}</Text>
         </Card>)}
 
