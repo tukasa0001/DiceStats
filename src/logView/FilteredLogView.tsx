aff39b34b7795c9d59f68aaf46942dfcbbcae24c
EmptyBottle <tts13511@hotmail.com>
2025-12-15 16:46:10 +0900

    発言者フィルターをMultiSelectに変更

--- src/logView/FilteredLogView.tsx
+++ src/logView/FilteredLogView.tsx
@@ -48,33 +48,29 @@ export const FilteredLogView = (props: FilteredLogViewProps) => {
                 selectAllLabel="全て表示"
                 emptyMessage="選択肢なし"
                 selectedItemsLabel="{0}個選択"
-                maxSelectedLabels={2}
+                maxSelectedLabels={1}
                 appendTo="self"
                 showSelectAll={false} />
-            <Select.Root defaultValue={UNFILTERED} onValueChange={sel => setFilter({ ...filter, hiddenMessageTypes: sel === UNFILTERED ? [] : allMessageTypes.filter(val => val !== sel) })}>
-                <Select.Trigger />
-                <Select.Content position="popper">
-                    <Select.Group>
-                        <Select.Item value={UNFILTERED}>全種類</Select.Item>
-                        {allMessageTypes
-                            .sort((a, b) => a[0].localeCompare(b[0], "ja"))
-                            .map((name, i) => <Select.Item key={i} value={name}>{ccfoliaMessageTypeTexts.get(name) || name}</Select.Item>)}
-                    </Select.Group>
-                </Select.Content>
-            </Select.Root>
             {/*発言者フィルター*/}
-            <Select.Root defaultValue={UNFILTERED} onValueChange={sel => setFilter({ ...filter, hiddenCharacters: sel === UNFILTERED ? [] : allCharacters.filter(val => val !== sel) })}>
-                <Select.Trigger />
-                <Select.Content position="popper">
-                    <Select.Group>
-                        <Select.Item value={UNFILTERED}>全員を表示</Select.Item>
-                        {allCharacters
-                            .map(str => str === "" ? "noname" : str)
-                            .sort((a, b) => a.localeCompare(b, "ja"))
-                            .map((name, i) => <Select.Item key={i} value={name}>{name}</Select.Item>)}
-                    </Select.Group>
-                </Select.Content>
-            </Select.Root>
+            <MultiSelect
+                value={reverseArray(filter.hiddenCharacters, allCharacters)}
+                onChange={(e) => setFilter({ ...filter, hiddenCharacters: reverseArray(e.value, allCharacters) })}
+                options={allCharacters
+                    .map(str => {
+                        return {
+                            name: ccfoliaMessageTypeTexts.get(str) || str,
+                            value: str.length === 0 ? "EMPTY" : str
+                        }
+                    })}
+                optionLabel="name"
+                optionValue="value"
+                placeholder="選択してください"
+                selectAllLabel="全て表示"
+                emptyMessage="選択肢なし"
+                selectedItemsLabel="{0}個選択"
+                maxSelectedLabels={1}
+                appendTo="self"
+                showSelectAll={false} />
             {/*内容検索*/}
             <TextField.Root value={filter.searchText} onChange={e => setFilter({ ...filter, searchText: e.target.value })} placeholder="検索">
                 <TextField.Slot />
