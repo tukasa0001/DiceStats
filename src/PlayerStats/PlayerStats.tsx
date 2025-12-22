6b006160f8f8d2f0f1b24f5216c5b01a98ce6702
EmptyBottle <tts13511@hotmail.com>
2025-12-22 16:52:53 +0900

    表示スタイルを追加

--- src/PlayerStats/PlayerStats.tsx
+++ src/PlayerStats/PlayerStats.tsx
@@ -7,7 +7,7 @@ import { CcfoliaMessage } from '../ccfoliaLog/message/CcfoliaMessage';
 import { UnknownSecretDiceMessage } from '../ccfoliaLog/message/UnknownSecretDiceMessage';
 import { configCtx, setConfigCtx } from '../App';
 import { ErrorBlock, InfoBlock } from '../Utils';
-import { Box, Button, ContextMenu, Dialog, Flex, Select, Table, Heading, TextField, CheckboxCards, Text } from '@radix-ui/themes';
+import { Box, Button, ContextMenu, Dialog, Flex, Select, Table, Heading, TextField, CheckboxCards, Text, Theme } from '@radix-ui/themes';
 import "./PlayerStats.css"
 
 class SkillStat {
@@ -66,7 +66,7 @@ type StatsProps = {
 
 const PlayerStats = (props: StatsProps) => {
     const [selectedCharacters, setSelectedCharacters] = useState<string[]>([]);
-    const [skillFilter, setSkillFinter] = useState("none");
+    const [playerName, setPlayerName] = useState("");
 
     const log = props.logFile;
     const config = useContext(configCtx);
@@ -162,6 +162,8 @@ const PlayerStats = (props: StatsProps) => {
         }
     }
 
+    const skillRanking = [...skillStats].sort((a, b) => b[1].skillRollNum - a[1].skillRollNum)
+
     const avgFormatter = Intl.NumberFormat("ja-JP", {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2
@@ -174,33 +176,144 @@ const PlayerStats = (props: StatsProps) => {
     });
 
     return (
-        <Box my="2">
-            <CheckboxCards.Root value={selectedCharacters}
-                onValueChange={val => setSelectedCharacters(val)}
-                columns={{ initial: "1", sm: "3" }}>
-                { }
-                <CheckboxCards.Item value="1">
-                    <Flex direction="column" width="100%">
-                        <Text weight="bold">A1 Keyboard</Text>
-                        <Text>US Layout</Text>
+        <Theme scaling="110%">
+            <Box my="2">
+                <Heading my="2">あなたの名前を入力してください</Heading>
+                <TextField.Root value={playerName} onChange={e => setPlayerName(e.target.value)} placeholder="名無しの探索者">
+                    <TextField.Slot />
+                </TextField.Root>
+                <Heading my="2">あなたのキャラを選択してください</Heading>
+                <CheckboxCards.Root value={selectedCharacters}
+                    onValueChange={val => setSelectedCharacters(val)}>
+                    {[...allCharacters].map(name => <CheckboxCards.Item key={name} value={name}>
+                        <Flex direction="column" width="100%">
+                            <Text weight="bold">{name}</Text>
+                        </Flex>
+                    </CheckboxCards.Item>)}
+                </CheckboxCards.Root>
+                <Flex id="playerStats" mt="9" py="2" gap="5" width="100%" height="100vh" direction="column" align="center">
+                    <Heading size="7">TRPG証書</Heading>
+                    <Text>{playerName === "" ? "名無しの探索者" : playerName} 殿</Text>
+                    <Text>上記の者は、TRPGにおいて以下の成績を修めたことを証する。</Text>
+                    <Flex justify="between">
+                        <Box flexGrow="1">
+                            <Heading align="center">多く振った技能</Heading>
+                            <Table.Root>
+                                <Table.Body>
+                                    <SkillRankingRow stats={skillRanking} rank={1} />
+                                    <SkillRankingRow stats={skillRanking} rank={2} />
+                                    <SkillRankingRow stats={skillRanking} rank={3} />
+                                </Table.Body>
+                            </Table.Root>
+                        </Box>
+                        <Box flexGrow="1">
+                            <Heading align="center">全技能</Heading>
+                            <Table.Root>
+                                <Table.Body>
+                                    <Table.Row>
+                                        <Table.RowHeaderCell>技能振り回数</Table.RowHeaderCell>
+                                        <Table.Cell>{sumOf(skillRanking.map(elem => elem[1].skillRollNum))}</Table.Cell>
+                                    </Table.Row>
+                                    <Table.Row>
+                                        <Table.RowHeaderCell>成功率</Table.RowHeaderCell>
+                                        <Table.Cell>{percentageFormatter.format(
+                                            sumOf(skillRanking.map(elem => elem[1].successNum)) / sumOf(skillRanking.map(elem => elem[1].skillRollNum))
+                                        )}</Table.Cell>
+                                    </Table.Row>
+                                    <Table.Row>
+                                        <Table.RowHeaderCell>クリティカル率</Table.RowHeaderCell>
+                                        <Table.Cell>{percentageFormatter.format(
+                                            sumOf(skillRanking.map(elem => elem[1].criticalNum)) / sumOf(skillRanking.map(elem => elem[1].skillRollNum))
+                                        )}</Table.Cell>
+                                    </Table.Row>
+                                    <Table.Row>
+                                        <Table.RowHeaderCell>ファンブル率</Table.RowHeaderCell>
+                                        <Table.Cell>{percentageFormatter.format(
+                                            sumOf(skillRanking.map(elem => elem[1].fumbleNum)) / sumOf(skillRanking.map(elem => elem[1].skillRollNum))
+                                        )}</Table.Cell>
+                                    </Table.Row>
+                                </Table.Body>
+                            </Table.Root>
+                        </Box>
                     </Flex>
-                </CheckboxCards.Item>
-                <CheckboxCards.Item value="2">
-                    <Flex direction="column" width="100%">
-                        <Text weight="bold">Pro Mouse</Text>
-                        <Text>Zero-lag wireless</Text>
+                    <Flex justify="between">
+                        <Flex direction="column">
+                            <Box className='valueBox'>
+                                <Text as="div" size="1">発言数</Text>
+                                <Text as="div" align="right">{talkStat.talkNum}回</Text>
+                            </Box>
+                            <Box className='valueBox'>
+                                <Text as="div" size="1">キャラ発言数</Text>
+                                <Text as="div" align="right">{talkStat.pcTalkNum}回</Text>
+                            </Box>
+                        </Flex>
+                        <Flex direction="column">
+                            <Box className='valueBox'>
+                                <Text as="div" size="1">総喪失HP</Text>
+                                <Text as="div" align="right">{statusStat.totalDamage}回</Text>
+                            </Box>
+                            <Box className='valueBox'>
+                                <Text as="div" size="1">総喪失SAN</Text>
+                                <Text as="div" align="right">{statusStat.totalLostSAN}回</Text>
+                            </Box>
+                        </Flex>
+                        <Flex direction="column">
+                            <Box className='valueBox'>
+                                <Text as="div" size="1">SANチェック回数</Text>
+                                <Text as="div" align="right">{sanityCheckStat.checkNum}回</Text>
+                            </Box>
+                            <Box className='valueBox'>
+                                <Text as="div" size="1">SANチェック成功率</Text>
+                                <Text as="div" align="right">{percentageFormatter.format(sanityCheckStat.successNum / sanityCheckStat.checkNum)}</Text>
+                            </Box>
+                        </Flex>
                     </Flex>
-                </CheckboxCards.Item>
-                <CheckboxCards.Item value="3">
-                    <Flex direction="column" width="100%">
-                        <Text weight="bold">Lightning Mat</Text>
-                        <Text>Wireless charging</Text>
-                    </Flex>
-                </CheckboxCards.Item>
-            </CheckboxCards.Root>
-
-        </Box>
+                </Flex>
+            </Box>
+        </Theme>
     );
+}
+
+const SkillRankingRow = (props: {
+    stats: [string, SkillStat][],
+    rank: number
+}) => {
+    const percentageFormatter = Intl.NumberFormat("ja-JP", {
+        style: "percent",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
+    const localeHanidec = Intl.NumberFormat(new Intl.Locale(
+        'ja-jp',
+        {
+            numberingSystem: "hanidec"
+        }
+    ));
+
+    const { stats, rank } = props;
+    if (rank < 1) {
+        return "Invalid rank: " + rank;
+    }
+
+    const stat = stats[rank - 1];
+    if (stat === undefined) {
+        return null;
+    }
+
+    return (<Table.Row>
+        <Table.RowHeaderCell>{localeHanidec.format(rank)}位</Table.RowHeaderCell>
+        <Table.Cell>{stat[0]}</Table.Cell>
+        <Table.Cell>{stat[1].skillRollNum}回</Table.Cell>
+        <Table.Cell><Text size="1">成功率</Text>{percentageFormatter.format(stat[1].successNum / stat[1].skillRollNum)}</Table.Cell>
+    </Table.Row>)
+}
+
+const sumOf = (array: number[]) => {
+    let sum = 0;
+    for (let elem of array) {
+        sum += elem;
+    }
+    return sum;
 }
 
 export default PlayerStats
