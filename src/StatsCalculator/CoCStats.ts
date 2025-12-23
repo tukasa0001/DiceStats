f3ade16a9b8c77d720b79a5c8f60de992f04bf8a
EmptyBottle <tts13511@hotmail.com>
2025-12-23 16:21:22 +0900

    CoC用の統一的なステータス計算処理を追加

---
+++ src/StatsCalculator/CoCStats.ts
@@ -0,0 +1,167 @@
+import { CcfoliaMessage } from "../ccfoliaLog/message/CcfoliaMessage";
+import { CoCSkillRollMessage } from "../ccfoliaLog/message/CoCSkillRollMessage";
+import { ParamChangeMessage } from "../ccfoliaLog/message/ParamChangeMessage";
+import { SanityCheckMessage } from "../ccfoliaLog/message/SanityCheckMessage";
+import { TalkMessage } from "../ccfoliaLog/message/TalkMessasge";
+
+class CoCStatsCounter {
+    createStat = (): CoCStat => ({
+        total: this.createCharacterStat(),
+        perCharacter: new Map()
+    })
+
+    createSkillStat = (): SkillStat => ({
+        rollNum: 0,
+        valueSum: 0,
+        successNum: 0,
+        failNum: 0,
+        criticalNum: 0,
+        spCriticalNum: 0,
+        fumbleNum: 0,
+        spFumbleNum: 0
+    })
+
+    createCharacterStat = (): CharacterStat => ({
+        skillRoll: { ...this.createSkillStat(), perSkill: new Map() },
+        sanityCheck: this.createSkillStat(),
+        status: {
+            totalDamage: 0,
+            minHealth: 0,
+            totalLostSAN: 0,
+            minSAN: 0,
+        },
+        talk: {
+            talkNum: 0,
+            charNum: 0,
+            pcTalkNum: 0,
+            pcCharNum: 0,
+        }
+    })
+
+    calc = (log: CcfoliaMessage[]) => {
+        let stat = this.createStat();
+        for (let msg of log) {
+            this.incrementStat(stat.total, msg);
+            if (msg.sender !== "") {
+                if (!stat.perCharacter.has(msg.sender)) {
+                    stat.perCharacter.set(msg.sender, this.createCharacterStat());
+                }
+                this.incrementStat(stat.perCharacter.get(msg.sender)!, msg);
+            }
+        }
+    }
+
+    incrementStat = (stat: CharacterStat, msg: CcfoliaMessage) => {
+        if (msg instanceof CoCSkillRollMessage) {
+            this.incrementSkillStat(stat.skillRoll, msg);
+            if (!stat.skillRoll.perSkill.has(msg.skill)) {
+                stat.skillRoll.perSkill.set(msg.skill, this.createSkillStat());
+            }
+            this.incrementSkillStat(stat.skillRoll.perSkill.get(msg.skill)!, msg);
+        }
+        else if (msg instanceof ParamChangeMessage) {
+            this.incrementStatusStat(stat, msg);
+        }
+        else if (msg instanceof SanityCheckMessage) {
+            this.incrementSkillStat(stat.sanityCheck, msg);
+        }
+        else if (msg instanceof TalkMessage) {
+            this.incrementTalkStat(stat, msg);
+        }
+    }
+
+    incrementSkillStat = (stat: SkillStat, msg: CoCSkillRollMessage | SanityCheckMessage) => {
+        stat.rollNum++;
+        stat.valueSum += msg.diceValue;
+        if (msg.isSuccess()) {
+            stat.successNum++;
+            if (msg.isCritical()) {
+                stat.criticalNum++;
+                if (msg.diceValue === 1) {
+                    stat.spCriticalNum++;
+                }
+            }
+        }
+        else {
+            stat.failNum++;
+            if (msg.isFumble()) {
+                stat.fumbleNum++;
+                if (msg.diceValue === 100) {
+                    stat.spFumbleNum++;
+                }
+            }
+        }
+    }
+
+    incrementStatusStat = (stat: CharacterStat, msg: ParamChangeMessage) => {
+        if (msg.paramName === "HP") {
+            // HP変動
+            if (stat.status.minHealth === undefined || msg.value < stat.status.minHealth) {
+                stat.status.minHealth = msg.value;
+            }
+            if (msg.value < msg.prevValue) {
+                // HP減少
+                stat.status.totalDamage += msg.prevValue - msg.value;
+            }
+        }
+        if (msg.paramName === "SAN") {
+            // SAN変動
+            if (stat.status.minSAN === undefined || msg.value < stat.status.minSAN) {
+                stat.status.minSAN = msg.value;
+            }
+            if (msg.value < msg.prevValue) {
+                // SAN減少
+                stat.status.totalLostSAN += msg.prevValue - msg.value;
+            }
+        }
+    }
+
+    incrementTalkStat = (stat: CharacterStat, msg: TalkMessage) => {
+        stat.talk.talkNum++;
+        stat.talk.charNum += msg.text.length;
+        const regex = msg.text.match(/^「(.*)」/);
+        if (regex !== null) {
+            stat.talk.pcTalkNum++;
+            stat.talk.pcCharNum += regex[1].length;
+        }
+    }
+}
+
+export type CoCStat = {
+    total: CharacterStat
+    perCharacter: Map<string, CharacterStat>
+};
+
+type CharacterStat = {
+    skillRoll: SkillStat & {
+        perSkill: Map<string, SkillStat>
+    },
+    sanityCheck: SkillStat,
+    status: {
+        totalDamage: number
+        minHealth: number
+        totalLostSAN: number
+        minSAN: number
+    },
+    talk: {
+        talkNum: number
+        charNum: number
+        pcTalkNum: number
+        pcCharNum: number
+    }
+}
+
+type SkillStat = {
+    rollNum: number
+    valueSum: number
+    successNum: number
+    failNum: number
+    criticalNum: number
+    spCriticalNum: number
+    fumbleNum: number
+    spFumbleNum: number
+}
+
+
+const cocstats = new CoCStatsCounter();
+export default cocstats;
