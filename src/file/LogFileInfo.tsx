1e67c9b3c010db8a2a6ccfa36c04c0d6ad60fbeb
EmptyBottle <tts13511@hotmail.com>
2025-12-25 23:36:15 +0900

    開始・終了位置を指定するUIを追加

--- src/file/LogFileInfo.tsx
+++ src/file/LogFileInfo.tsx
@@ -1,5 +1,6 @@
-import { Box, Card, Text } from "@radix-ui/themes"
+import { Box, Button, Card, Flex, Text, TextField } from "@radix-ui/themes"
 import { LogFile } from "./LogFile"
+import { useState } from "react"
 
 type LogFileInfoProps = {
     log: LogFile
@@ -7,9 +8,42 @@ type LogFileInfoProps = {
 
 export const LogFileInfo = (props: LogFileInfoProps) => {
     const { log } = props;
+    const [startMsg, setStartMsg] = useState("");
+    const [endMsg, setEndMsg] = useState("");
+
     return <Box>
         <Card>
             <Text as="div" weight="bold">{log.filename}</Text>
+            <Flex ml="4" my="1" direction="column" gap="2">
+                <Flex gap="2" align="center">
+                    <Text>
+                        開始位置：
+                    </Text>
+                    <TextField.Root
+                        value={startMsg}
+                        onChange={e => {
+                            setStartMsg(e.target.value.trim())
+                        }}
+                        placeholder="最初から">
+                        <TextField.Slot />
+                    </TextField.Root>
+                    <Button variant="outline">選択</Button>
+                </Flex>
+                <Flex gap="2" align="center">
+                    <Text>
+                        終了位置：
+                    </Text>
+                    <TextField.Root
+                        value={endMsg}
+                        onChange={e => {
+                            setEndMsg(e.target.value.trim())
+                        }}
+                        placeholder="最後まで">
+                        <TextField.Slot />
+                    </TextField.Root>
+                    <Button variant="outline">選択</Button>
+                </Flex>
+            </Flex>
         </Card>
     </Box>
 }
