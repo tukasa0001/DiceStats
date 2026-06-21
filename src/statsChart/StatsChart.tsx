99718fe46d0e80d40c945e708c406a7deb915b63
EmptyBottle <tts13511@hotmail.com>
2026-06-21 17:55:48 +0900

    複数の種類のグラフを出せるよう変更

--- src/statsChart/StatsChart.tsx
+++ src/statsChart/StatsChart.tsx
@@ -1,9 +1,9 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { LogFile } from "../file/LogFile"
 import { configCtx } from "../App";
-import { Text, Box, Button, ContextMenu, Dialog, Flex, Select, Table, Heading, TextField } from '@radix-ui/themes';
+import { Text, Box, Button, ContextMenu, Dialog, Flex, Select, Table, Heading, TextField, ScrollArea, RadioCards } from '@radix-ui/themes';
 import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
-import cocstats, { CoCStat } from "../StatsCalculator/CoCStats";
+import cocstats, { CharacterStat, CoCStat } from "../StatsCalculator/CoCStats";
 
 const colors = [
     ...Array.from({ length: 12 }, (_, i) => i)
@@ -26,9 +26,20 @@ type DataPoint = {
     fumbleNum: number,
 }
 
+type ValueDisplay = (stat: CharacterStat) => number;
+
+const valueDisplays: ValueDisplay[] = [
+    stat => stat.skillRoll.rollNum,
+    stat => stat.skillRoll.successNum,
+    stat => stat.skillRoll.criticalNum,
+    stat => stat.skillRoll.fumbleNum,
+]
+
 const StatsChart = (props: StatsChartProps) => {
     const { logs } = props;
     const config = useContext(configCtx);
+
+    const [valueDisplay, setValueDisplay] = useState<ValueDisplay>(() => valueDisplays[0]);
 
     const jpnTextComparer = (a: string, b: string) => a.localeCompare(b, "ja");
 
@@ -64,7 +75,7 @@ const StatsChart = (props: StatsChartProps) => {
         name: `${i * 10}%`,
         ...allCharacters.map(name => ({ [name]: 0 })).reduce((a, b) => ({ ...a, ...b }), {}),
         ...[...stat.perCharacter].map(([name, stat]) => ({
-            [name]: stat.skillRoll.rollNum
+            [name]: valueDisplay(stat)
         })).reduce((a, b) => ({ ...a, ...b }), {})
     }));
 
@@ -73,22 +84,51 @@ const StatsChart = (props: StatsChartProps) => {
     return (
         <Box my="2">
             <Heading my="4">技能振り統計</Heading>
-            <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: "60vw" }} responsive data={data}>
-                <CartesianGrid strokeDasharray="3 3" stroke="var(--gray-4)" />
-                <XAxis dataKey="name" stroke="var(--accent-9)" />
-                <YAxis width="auto" stroke="var(--accent-9)" />
-                <Tooltip contentStyle={{
-                    opacity: 0.7,
-                    backgroundColor: 'var(--gray-2)',
-                    borderColor: 'var(--gray-6)',
-                }} />
-                <Legend />
-                {allCharacters.map((name, i) => <Line
-                    key={name} dataKey={name}
-                    stroke={colors[i]}
-                />)}
-            </LineChart>
-        </Box>
+            <Flex direction="row">
+                <RadioCards.Root defaultValue="0" onValueChange={val => {
+                    const idx = Number(val);
+                    setValueDisplay(() => valueDisplays[idx]);
+                }}>
+                    <Flex direction="column" gap="2">
+                        <RadioCards.Item value="0">
+                            <Flex direction="column" width="100%">
+                                <Text weight="bold">技能ロール</Text>
+                            </Flex>
+                        </RadioCards.Item>
+                        <RadioCards.Item value="1">
+                            <Flex direction="column" width="100%">
+                                <Text weight="bold">技能成功</Text>
+                            </Flex>
+                        </RadioCards.Item>
+                        <RadioCards.Item value="2">
+                            <Flex direction="column" width="100%">
+                                <Text weight="bold">クリティカル</Text>
+                            </Flex>
+                        </RadioCards.Item>
+                        <RadioCards.Item value="3">
+                            <Flex direction="column" width="100%">
+                                <Text weight="bold">ファンブル</Text>
+                            </Flex>
+                        </RadioCards.Item>
+                    </Flex>
+                </RadioCards.Root>
+                <LineChart style={{ flexGrow: 1, aspectRatio: 1.618, maxWidth: "80vw", maxHeight: "80vh" }} responsive data={data}>
+                    <CartesianGrid strokeDasharray="3 3" stroke="var(--gray-4)" />
+                    <XAxis dataKey="name" stroke="var(--accent-9)" />
+                    <YAxis width="auto" stroke="var(--accent-9)" />
+                    <Tooltip contentStyle={{
+                        opacity: 0.7,
+                        backgroundColor: 'var(--gray-2)',
+                        borderColor: 'var(--gray-6)',
+                    }} />
+                    <Legend />
+                    {allCharacters.map((name, i) => <Line
+                        key={name} dataKey={name}
+                        stroke={colors[i]}
+                    />)}
+                </LineChart>
+            </Flex>
+        </Box >
     )
 }
 
