d04b3e8cc91e6682b80657235ae837e1f4268214
EmptyBottle <tts13511@hotmail.com>
2026-06-25 14:56:18 +0900

    メッセージ表示をCcfoliaに近いものに変更

--- src/logView/LogView.tsx
+++ src/logView/LogView.tsx
@@ -1,4 +1,4 @@
-import { Badge, Box, Card, Code, Flex, ScrollArea } from "@radix-ui/themes";
+import { Badge, Box, Button, Card, Code, Flex, ScrollArea } from "@radix-ui/themes";
 import { CcfoliaMessage } from "../ccfoliaLog/message/CcfoliaMessage";
 import { Heading, Text } from "@radix-ui/themes";
 import { TalkMessage } from "../ccfoliaLog/message/TalkMessasge";
@@ -7,7 +7,7 @@ import { SanityCheckMessage } from "../ccfoliaLog/message/SanityCheckMessage";
 import { ParamChangeMessage } from "../ccfoliaLog/message/ParamChangeMessage";
 import LogViewFilter, { EMPTY_FILTER } from "./LogViewFilter";
 import { LogFile } from "../file/LogFile";
-import { useRef } from "react";
+import { CSSProperties, useRef } from "react";
 import { useVirtualizer } from "@tanstack/react-virtual";
 
 type LogViewProps = {
@@ -40,57 +40,76 @@ export const LogView = (props: LogViewProps) => {
     const virtualizer = useVirtualizer({
         count: filteredLog.length,
         getScrollElement: () => scrollAreaRef.current,
-        estimateSize: () => 72,
+        estimateSize: () => 81.6,
         overscan: 20,
-        gap: 16,
     })
 
     return (
-        <ScrollArea scrollbars="vertical" ref={scrollAreaRef} style={{
+        <ScrollArea scrollbars="vertical" size="2" ref={scrollAreaRef} style={{
             maxHeight: "100%"
         }}>
             <Box mt="4" height={`${virtualizer.getTotalSize()}px`} position="relative" ref={virtualizer.containerRef}>
                 {virtualizer.getVirtualItems().map(vItem => (
-                    <Card key={vItem.key} data-index={vItem.index}
-                        ref={virtualizer.measureElement} asChild={onClick !== undefined}
-                        style={{
-                            position: 'absolute',
-                            top: 0,
-                            left: 0,
-                            width: '100%',
-                            transform: `translateY(${vItem.start}px)`,
-                        }}>
-                        {onClick === undefined
-                            // 通常
-                            ? <MessageCardContent msg={filteredLog[vItem.index]} filter={filter} />
-                            // クリック可能
-                            : <button onClick={() => onClick(filteredLog[vItem.index], vItem.index)}>
-                                <MessageCardContent msg={filteredLog[vItem.index]} filter={filter} />
-                            </button>}
-                    </Card>
-
+                    <Box key={vItem.key} data-index={vItem.index} style={{
+                        width: "100%",
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        transform: `translateY(${vItem.start}px)`,
+                    }} ref={virtualizer.measureElement}>
+                        <MessageEntry
+                            msg={filteredLog[vItem.index]} filter={filter}
+                            onClick={onClick ? () => onClick(filteredLog[vItem.index], vItem.index) : undefined}
+                        />
+                    </Box>
                 ))}
             </Box>
         </ScrollArea>
     )
 };
 
-const MessageCardContent = (props: { msg: CcfoliaMessage, filter: LogViewFilter }) => {
-    const { msg, filter } = props;
+const MessageEntry = (props: {
+    msg: CcfoliaMessage, filter: LogViewFilter,
+    onClick?: () => void
+}) => {
+    const { msg, filter, onClick } = props;
+
+    const blockStyle: CSSProperties = {
+        display: "block"
+    }
+
     return <>
-        <Flex gap="2" direction="row">
-            <Heading size="4">{msg.sender}</Heading>
-            {msg instanceof CoCSkillRollMessage ? <Badge color="lime">技能判定</Badge> : null}
-            {msg instanceof SanityCheckMessage ? <Badge color="ruby">SANチェック</Badge> : null}
-            {msg instanceof ParamChangeMessage ? <Badge color="blue">{msg.paramName}変動</Badge> : null}
-        </Flex>
-        {
-            filter.searchText === "" ? <Text>{msg.toDisplayText()}</Text> : <Text>
-                {msg.toDisplayText().split(filter.searchText)
-                    .map((text, i) => i === 0 ? text : <>
-                        <u style={{ textDecorationColor: "lime" }}>{filter.searchText}</u>{text}
-                    </>)}
-            </Text>
-        }
+        <Box px="4" py="2" position="relative">
+            <Flex gap="2" direction="row">
+                <Heading size="4">{msg.sender}</Heading>
+                {msg instanceof CoCSkillRollMessage ? <Badge color="lime">技能判定</Badge> : null}
+                {msg instanceof SanityCheckMessage ? <Badge color="ruby">SANチェック</Badge> : null}
+                {msg instanceof ParamChangeMessage ? <Badge color="blue">{msg.paramName}変動</Badge> : null}
+            </Flex>
+            {filter.searchText === "" ? <Text style={blockStyle}>{msg.toDisplayText()}</Text> :
+                <Text style={blockStyle}>
+                    {msg.toDisplayText().split(filter.searchText)
+                        .map((text, i) => i === 0 ? text : <>
+                            <u style={{ textDecorationColor: "lime" }}>{filter.searchText}</u>{text}
+                        </>)}
+                </Text>
+            }
+
+            {onClick === undefined ? null : (
+                <Button variant="ghost" onClick={onClick} style={{
+                    position: "absolute",
+                    bottom: "1em",
+                    right: "1.5em",
+                }}>
+                    選択
+                </Button>
+            )}
+        </Box>
+
+        <hr style={{
+            borderColor: "#3f3f3f",
+            marginLeft: "var(--space-4)",
+            marginRight: "var(--space-4)",
+        }} />
     </>
 }
